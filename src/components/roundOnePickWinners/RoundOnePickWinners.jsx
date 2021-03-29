15a803a6cc7872a1f231908a44b765287c05df9f
ErikGraciosa <erikgraciosa@gmail.com>
2021-03-28 18:45:30 -0700

    using css to persist color on checked

--- src/components/roundOnePickWinners/RoundOnePickWinners.jsx
+++ src/components/roundOnePickWinners/RoundOnePickWinners.jsx
@@ -26,14 +26,30 @@ function RoundOnePickWinners({
       <form onSubmit={submitResults}>
         <header>Pick Winners Game 1 - {gameOne}</header>
         <div className={styles.results}>
-          <div className={styles.chooseWinner}>
+          <input
+            type="radio"
+            className={styles.chooseWinner}
+            name="game-one"
+            id="away-team-game-one"
+            hidden/>
+          <label
+            htmlFor="away-team-game-one"
+            className={styles.chooseWinner}>
             <p>{playerOne}</p>
             <p>{playerTwo}</p>
-          </div>
-          <div className={styles.chooseWinner}>
+          </label>
+          <input
+            type="radio"
+            className={styles.chooseWinner}
+            name="game-one"
+            id="home-team-game-one"
+            hidden/>
+          <label
+            htmlFor="home-team-game-one"
+            className={styles.chooseWinner}>
             <p>{playerFive}</p>
             <p>{playerSix}</p>
-          </div>
+          </label>
         </div>
         <header>Pick Winners Game 2 - {gameTwo}</header>
         <div className={styles.results}>
@@ -42,7 +58,7 @@ function RoundOnePickWinners({
             className={styles.chooseWinner}
             name="game-two"
             id="away-team-game-two"
-            hidden></input>
+            hidden/>
           <label
             htmlFor="away-team-game-two"
             className={styles.chooseWinner}>
@@ -54,14 +70,13 @@ function RoundOnePickWinners({
             className={styles.chooseWinner}
             name="game-two"
             id="home-team-game-two"
-            hidden></input>
+            hidden/>
           <label
             htmlFor="home-team-game-two"
             className={styles.chooseWinner}>
             <p>{playerSeven}</p>
             <p>{playerEight}</p>
           </label>
-          
         </div>
         <button>Submit Results</button>
       </form>
