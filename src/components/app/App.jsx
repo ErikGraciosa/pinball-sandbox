856d26d3997f48eae4a550c6fea032770589db7d
ErikGraciosa <erikgraciosa@gmail.com>
2021-03-30 20:55:38 -0700

    added the stats pages to router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,6 +4,7 @@ import DataEntry from '../../containers/dataEntry/DataEntry';
 import RoundOneFlow from '../../containers/roundOneFlow/RoundOneFlow';
 import './reset.css';
 import './App.css';
+import RoundOneStats from '../../containers/roundOneStats/RoundOneStats';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Switch>
           <Route exact path="/" component={RoundOneFlow}/>
           <Route exact path="/dataentry" component={DataEntry}/>
+          <Route exact path="/roundOneStats" component={RoundOneStats}/>
         </Switch>
       </Router>
     </>
